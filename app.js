a7386992497624cf34626450882070ba0c4fd059
haval <havalkoet00908@gmail.com>
2022-09-01 02:05:05 -0700

    conect mongodb with ouer todolist app

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose=require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema= new mongoose.Schema({
+  name:String,
+});
+const Item=mongoose.model("Item",itemsSchema);
+
+const item1 =new Item({
+  name:"welcome to  your todo list ",
+});
+const item2=new Item({
+  name:"hit the + button to add a new item."
+});
+const item3=new Item({
+  name:"<-- hit this to dlete an item."
+});
+
+
+const defaultItems=[item1,item2,item3];
+
+
+
+app.get("/", function(req, res) {
+
+  Item.find({},(err,docs)=>{
+    if ( docs.length === 0 ){
+        Item.insertMany(defaultItems,(err)=>{
+            if (err){
+              comsole.log(err)
+            }else{
+              console.log("successfully saved default itemsto DB")
+            }
+            res.redirect("/");
+      });       
+    }else{
+      res.render("list", {listTitle: day, newListItems: docs});
+    }
+  });
+
+const day = date.getDate();
+
+  
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
